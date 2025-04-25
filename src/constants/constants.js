399c7d9f07900c2599e16780fc29290db7b41bf7
Aliaksandra <alekspekhota@gmail.com>
2025-04-25 16:10:28 +0200

    feat: add create file functionality

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+// FS
+export const FS_ERROR_MESSAGE = 'FS operation failed'
+export const FS_CREATE_FILE_NAME = 'fresh.txt'
+export const FS_CREATE_FILE_CONTENT = 'I am fresh and young'
+export const FS_FOLDER_NAME = 'files'
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile } from 'fs/promises'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-await create();
+import {
+  FS_CREATE_FILE_CONTENT,
+  FS_CREATE_FILE_NAME,
+  FS_FOLDER_NAME,
+  FS_ERROR_MESSAGE
+} from '../constants/constants.js'
+import { checkIsExistingFile } from '../helpers/checkIsExistingFile.js'
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
+
+const create = async () => {
+  const filePath = join(__dirname, FS_FOLDER_NAME, FS_CREATE_FILE_NAME)
+
+  const isExistingFile = await checkIsExistingFile(filePath)
+
+  if (isExistingFile) {
+    throw new Error(FS_ERROR_MESSAGE)
+  }
+
+  try {
+    await writeFile(filePath, FS_CREATE_FILE_CONTENT)
+    console.info('File was created')
+  } catch (err) {
+    throw new Error(FS_ERROR_MESSAGE)
+  }
+}
+
+await create()
+
