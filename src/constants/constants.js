e2a6ebc03a7f7f7b3d88fea4b3fd1bfa9a5b182d
Aliaksandra <alekspekhota@gmail.com>
2025-04-25 17:54:03 +0200

    feat: implement rename functionality

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,4 +5,6 @@ export const FS_CREATE_FILE_CONTENT = 'I am fresh and young'
 export const FS_FOLDER_NAME = 'files'
 
 export const FS_COPY_FOLDER_NAME = 'files_copy'
+export const WRONG_FILE_NAME = 'wrongFilename.txt'
+export const PROPER_FILE_NAME = 'properFilename.md'
 
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import fs from 'fs/promises'
+
+import { FS_ERROR_MESSAGE, FS_FOLDER_NAME, PROPER_FILE_NAME, WRONG_FILE_NAME } from '../constants/constants.js';
+import { checkIsExistingFile } from '../helpers/checkIsExistingFile.js';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFileName = join(__dirname, FS_FOLDER_NAME, WRONG_FILE_NAME)
+    const properFileName = join(__dirname, FS_FOLDER_NAME, PROPER_FILE_NAME)
+
+    const isExistingProperFile = await checkIsExistingFile(properFileName)
+
+    if(isExistingProperFile) {
+        throw new Error(FS_ERROR_MESSAGE)
+    }
+
+    try {
+       await fs.rename(wrongFileName, properFileName) 
+    } catch (error) {
+        throw new Error(FS_ERROR_MESSAGE)
+    }
 };
 
 await rename();
