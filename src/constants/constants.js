95c908ebe1ac7bd7020615663bdde684df1fd472
Aliaksandra <alekspekhota@gmail.com>
2025-04-25 18:50:24 +0200

    implement delete functionality

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,8 +3,9 @@ export const FS_ERROR_MESSAGE = 'FS operation failed'
 export const FS_CREATE_FILE_NAME = 'fresh.txt'
 export const FS_CREATE_FILE_CONTENT = 'I am fresh and young'
 export const FS_FOLDER_NAME = 'files'
-
 export const FS_COPY_FOLDER_NAME = 'files_copy'
 export const WRONG_FILE_NAME = 'wrongFilename.txt'
 export const PROPER_FILE_NAME = 'properFilename.md'
+export const FILE_TO_REMOVE_NAME = 'fileToRemove.txt'
+
 
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { rm } from 'fs/promises'
 
-await remove();
+import {
+  FILE_TO_REMOVE_NAME,
+  FS_ERROR_MESSAGE,
+  FS_FOLDER_NAME
+} from '../constants/constants.js'
+import { findPath } from '../helpers/findPath.js'
+
+const remove = async () => {
+  const fileToRemovePath = findPath(import.meta.url, FS_FOLDER_NAME, FILE_TO_REMOVE_NAME)
+
+  try {
+    await rm(fileToRemovePath)
+  } catch (error) {
+    throw new Error(FS_ERROR_MESSAGE)
+  }
+}
+
+await remove()
