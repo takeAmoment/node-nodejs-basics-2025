ce74e8a1bd81795226b39e7d6f9134d7cc4b0424
Aliaksandra <alekspekhota@gmail.com>
2025-04-25 20:26:48 +0200

    refactor: rewrite module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,46 @@
+import path from 'path'
+import { release, version } from 'os'
+import { createServer as createServerHttp } from 'http'
+import { readFile } from 'fs/promises'
+import { fileURLToPath } from 'url'
+
+import './files/c.js'
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
+
+const a = JSON.parse(await readFile(new URL('./files/a.json', import.meta.url)))
+const b = JSON.parse(await readFile(new URL('./files/b.json', import.meta.url)))
+
+const random = Math.random()
+
+let unknownObject
+
+if (random > 0.5) {
+  unknownObject = a
+} else {
+  unknownObject = b
+}
+
+console.log(`Release ${release()}`)
+console.log(`Version ${version()}`)
+console.log(`Path segment separator is "${path.sep}"`)
+
+console.log(`Path to current file is ${__fileName}`)
+console.log(`Path to current directory is ${__dirname}`)
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted')
+})
+
+const PORT = 3000
+
+console.log(unknownObject)
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+  console.log('To terminate it, use Ctrl+C combination')
+})
+
+export { unknownObject, myServer }
+
