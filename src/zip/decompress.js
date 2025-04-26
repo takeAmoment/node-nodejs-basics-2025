c34f85b536d86fcaeba2fb82834ace22024c3dac
Aliaksandra <alekspekhota@gmail.com>
2025-04-26 11:55:31 +0200

    feat: add decompress functionality

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,44 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream/promises'
+import { createUnzip } from 'zlib'
 
-await decompress();
+import {
+  COMMON_ERROR_MESSAGE,
+  ZLIB_COMPESSED_FILE_NAME,
+  ZLIB_FILE_TO_COMPRESS,
+  ZLIB_FOLDER_NAME
+} from '../constants/constants.js'
+import { findPath } from '../helpers/findPath.js'
+import { checkIsExistingFile } from '../helpers/checkIsExistingFile.js'
+
+const decompress = async () => {
+  const fileToCompressPath = findPath(
+    import.meta.url,
+    ZLIB_FOLDER_NAME,
+    ZLIB_FILE_TO_COMPRESS
+  )
+  const compessedFilePath = findPath(
+    import.meta.url,
+    ZLIB_FOLDER_NAME,
+    ZLIB_COMPESSED_FILE_NAME
+  )
+
+  const isExistingSourceFile = await checkIsExistingFile(compessedFilePath)
+
+  if (!isExistingSourceFile) {
+    throw new Error(COMMON_ERROR_MESSAGE)
+  }
+
+  const source = createReadStream(compessedFilePath)
+  const destination = createWriteStream(fileToCompressPath)
+  const unGzip = createUnzip()
+
+  try {
+    await pipeline(source, unGzip, destination)
+  } catch (error) {
+    throw new Error(COMMON_ERROR_MESSAGE)
+  }
+}
+
+await decompress()
+
