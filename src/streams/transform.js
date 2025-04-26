5a4c03e4924977286b2f1a4a9b57fbdaf020ed12
Aliaksandra <alekspekhota@gmail.com>
2025-04-26 11:27:28 +0200

    feat: implement transform functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import process from 'process'
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+import { COMMON_ERROR_MESSAGE } from '../constants/constants.js';
+
+const transformData = new Transform({
+    transform(chunk, _, cb) {
+        const reverseText = chunk.toString().split('').reverse().join('')
+        this.push(reverseText)
+        cb()
+    },
+  }); 
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        await pipeline(process.stdin, transformData, process.stdout)
+    } catch (error) {
+        throw new Error(COMMON_ERROR_MESSAGE)
+    }
 };
 
 await transform();
