9a0d46c46cd8c26f2128f95053609615ed609a30
Aliaksandra <alekspekhota@gmail.com>
2025-04-28 15:54:23 +0200

    refactor: add findPath func to create fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,4 @@
 import { writeFile } from 'fs/promises'
-import { join, dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 import {
   FS_CREATE_FILE_CONTENT,
@@ -9,12 +7,11 @@ import {
   FS_ERROR_MESSAGE
 } from '../constants/constants.js'
 import { checkIsExistingFile } from '../helpers/checkIsExistingFile.js'
+import { findPath } from '../helpers/findPath.js'
 
-const __fileName = fileURLToPath(import.meta.url)
-const __dirname = dirname(__fileName)
 
 const create = async () => {
-  const filePath = join(__dirname, FS_FOLDER_NAME, FS_CREATE_FILE_NAME)
+  const filePath = findPath(import.meta.url, FS_FOLDER_NAME, FS_CREATE_FILE_NAME)
 
   const isExistingFile = await checkIsExistingFile(filePath)
 
